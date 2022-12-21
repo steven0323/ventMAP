4ea96b52aad890454ea1525d996a7d70be733800
steven0323 <Steph199603@gmail.com>
2022-12-20 19:56:56 -0800

    update comment

--- anonymize_cohort.sh
+++ anonymize_cohort.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-# Please specify patient_directory and the place you want to store info.csv before you run the script in default.yaml
+# Please specify the path to your unanonymized dataset before you run the script in default.yaml
 
 unanon_data_path="${UNANON_DATA_PATH:-$(yq ".unanon_data_path" default.yaml)}" 
 anon_data_path="${ANON_DATA_PATH:-$(yq ".anon_data_path" default.yaml)}" 
 
 export raw_patient_dir data_path
 
+# anonymize_rawdata - anonymize raw data and store info.csv as mapping file (real id <> anonymized_id)
 anonymize_rawdata(){
     echo "Start anonymizing raw data ..."
     for dir in ${unanon_data_path}/experiment1/all_data/raw/*/; do
@@ -17,6 +18,7 @@ anonymize_rawdata(){
     echo "Anonymization on raw data finished !!"
 }
 
+# update_cohort_description - anonymize cohort description 
 update_cohort_description(){
     echo "Update cohort description ..."
     python ventmap/redo_cohort_desc_after_anonymization.py --shift-file $anon_data_path/info.csv --non-anon-cohort-desc $unanon_data_path/cohort-description.csv --anon-cohort-desc $anon_data_path/anon-desc.csv
@@ -24,6 +26,7 @@ update_cohort_description(){
 
 }
 
+# conda_activate - activate conda environment for machine learning model training
 conda_activate(){
     # Need to specify the path to ccil_vwd here if not in Desktop
     echo "conda activate ards ..."
@@ -32,6 +35,7 @@ conda_activate(){
     source activate ards
 }
 
+# baseline - generate pickle file for training dataset and run baseline
 baseline(){
     echo "Preprocessed dataset and run baseline ..."    
     python train.py --data-path $anon_data_path --cohort-description $anon_data_path/anon-desc.csv --to-pickle processed_dataset.pkl
